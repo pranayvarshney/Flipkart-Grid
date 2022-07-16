16bcb7a956fb9ada7f5429b9c69d0a89aa074d1a
Yuvraj <mryuvrajsbhadauria@gmail.com>
2022-07-16 17:14:54 +0530

    contract code skeleton

--- contracts/contract_v1.sol
+++ contracts/contract_v1.sol
@@ -13,53 +13,114 @@ contract Product is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, 
 
     Counters.Counter private _tokenIdCounter;
 
-    mapping(string => string) serialToCust;
-    // enterprise will populate this mapping
+    constructor() ERC721("Product", "PDCT") {}
 
-    mapping(address => uint256[]) ownerToItems;
-    //map of owner to his NFTs, will be used to show list of his items on frontend
+    //Struct for Purchasing History
+    struct purchasingHistory{
+        address ownerAdd;
+        uint purchaseDate;
+    }
 
-    //flipkart address
-    address FLIPKART_ADDRESS = 0x7521bF23C427Ca52016Fda4709932C56D23aa487;
-    modifier onlyFlipkart() {
-        require(msg.sender == FLIPKART_ADDRESS);    
+    //Struct for warranty claims
+    struct warrantyClaim{
+        enum status{ OPEN, CLOSE };
+        uint32 dateOfTicketOpening;
+        uint32 dateOfTicketClosing;
+        string complaint;
+        string redressal;
+    }
+
+    //mapping for Purchasing history
+    //todo: think about access modifier
+    mapping(string => purchasingHistory[]) public serialNumToPurchasingHistory;
+
+    //mapping for warranty
+    //todo: think about access modifier
+    mapping(string => warrantyClaim[]) public serialNumToWarrantyClaims;
+
+    //mapping for serialNum to current warranty period
+    //todo: think about access modifier
+    mapping(string => uint) public serialNumToCurrentWarrantyPeriod;
+ 
+    //Event to notify minting is completed
+    //todo: what values to emit??
+    event NotifyMint(address);
+
+    // modifier to check if warranty can be claimed or not
+    modifier isValid(string memory serialNum){
+        require(serialNumToPurchasingHistory[serialNum][0].purchaseDate + serialNumToCurrentWarrantyPeriod[serialNum] >= now );
         _;
     }
 
-    struct Item {
-        string serialNumber;
-        string productName;
-        string modelNo;
-        uint256 dateOfPurchase;
-        uint256 dateOfExpiryOfWarranty;
+    // to check the validity of warranty
+    function checkValidity(string memory serialNum) public returns (bool){
+        if(serialNumToPurchasingHistory[serialNum][0].purchaseDate + serialNumToCurrentWarrantyPeriod[serialNum] >= now){ 
+        return true;
+        }else{
+        return false;
+        }
     }
 
-    constructor() ERC721("Product", "PDCT") {}
-
-    function populateMap(string memory serialNumber, string memory custId) public onlyFlipkart {
-        serialToCust[serialNumber] = custId;
-    }
-    //will be called by flipkart when order is placed
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://someURI/";
-    }
-
-    function safeMint(address to, string memory uri, string memory serialNumber, string memory custId) public {
-        require(keccak256(abi.encodePacked(serialToCust[serialNumber])) == keccak256(abi.encodePacked(custId)), "You are not authorized to mint this NFT");
+    //Custom mint function
+    function safeMint(address to, string memory uri, string memory serialNum, uint memory warrantyPeriod, uint memory purchaseDate) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-        ownerToItems[to].push(tokenId);
+        serialNumToPurchasingHistory[serialNum].push(purchasingHistory(to,purchaseDate));
+        serialNumToCurrentWarrantyPeriod[serialNum] = warrantyPeriod;
+        emit NotifyMint(to);
     }
 
-    function listNFTsOfOwner(address _owner) public view onlyOwner returns (uint[] memory) {
-        return ownerToItems[_owner];
+    //Function to see the list of all the items owned by a customer
+    //todo: think about access modifier
+    function showMyItems(address memory owner) view external returns(string[] calldata){
+        string[] memory listOfTokenURIs; 
+        for(int i=0; i < balanceOf(owner); i++){
+            listOfTokenURIs.push(tokenURI(tokenOfOwnerByIndex(i)));
+        }
+        return listOfTokenURIs;
     }
+
+    // get purchasing history by serial number
+    function getPurchasingHistory(string memory serialNum) view external returns(purchasingHistory[] calldata){
+        return serialNumToPurchasingHistory[serialNum];
+    }
+
+    // get warranty claims history by serial number
+    function getWarrantyClaims(string memory serialNum) view external isValid(serialNum) returns(warrantyClaim[] calldata){
+        return serialNumToWarrantyClaims[serialNum];
+    }
+
+    //customer claims the warranty
+    function claimWarranty(string memory serialNum, string memory complaint) public {
+        serialNumToWarrantyClaims[serialNum].push(warranty(status.OPEN, now, 0, complaint, ""));
+    }
+
+    //Enterprise closes the warranty claim
+    function closeWarrantyTicket(string memory serialNum, string memory redressal) public {
+        serialNumToWarrantyClaims[serialNum][serialNumToWarrantyClaims.length - 1].status = status.CLOSE;
+        serialNumToWarrantyClaims[serialNum][serialNumToWarrantyClaims.length - 1].dateOfTicketClosing = now;
+        serialNumToWarrantyClaims[serialNum][serialNumToWarrantyClaims.length - 1].redressal = redressal;
+    }
+
+    //Event for transfer of ownership
+    event Transfer(address from, address to, uint256 tokenId)
+    // transfer ownership 
+    function transferOwnership(address memory to, uint memory tokenId, string memory serialNum){
+        _beforeTokenTransfer(msg.sender, to, tokenId);
+        safeTransferFrom(msg.sender, to, tokenId);
+        serialNumToPurchasingHistory[serialNum].push(purchasingHistory(to,now));
+        emit Transfer(msg.sender, to, tokenId);
+    }
+
+    //function to extend warranty
+    function extendWarranty(string memory serialNum, uint memory newWarrantyPeriod) public{
+        serialNumToCurrentWarrantyPeriod[serialNum] = newWarrantyPeriod;
+    }
+
 
     // The following functions are overrides required by Solidity.
-
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         override(ERC721, ERC721Enumerable)
